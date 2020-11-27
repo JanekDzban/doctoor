c638984befd60c7821767a819bb153282f0570f7
Karol <rpg-7@o2.pl>
2020-11-27 20:18:22 +0100

    MedicinesReminderIntentHandler implemantation

---
+++ lambda/intents/MedicinesReminderIntentHandler.js
@@ -0,0 +1,18 @@
+const MedicinesRemidnerIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'MedicinesRemidnerIntentHandler';
+        console.log('MedicinesRemidnerIntentHandler');
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+module.exports.HelloWorldIntentHandler = function get () {
+    return HelloWorldIntentHandler;
+}
