26def026852b282e430afc3fa2d45bae847a568c
Karol <rpg-7@o2.pl>
2020-11-27 23:42:42 +0100

    different approach to permissions

--- lambda/intents/MedicinesReminderIntentHandler.js
+++ lambda/intents/MedicinesReminderIntentHandler.js
@@ -6,14 +6,30 @@ const MedicinesReminderIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MedicinesReminderIntent';
     },
     handle(handlerInput) {
-        const reminderApiClient = handlerInput.serviceClientFactory.getReminderManagementServiceClient(),
+        /*const reminderApiClient = handlerInput.serviceClientFactory.getReminderManagementServiceClient(),
             {permisions} = handlerInput.requestEnvelope.context.System.user;
         if(!permisions) {
             return handlerInput.responseBuilder
                 .speak("Please go to the Alexa mobile app to grant reminders permissions.")
                 .withAskForPermissionsConsentCard(['alexa::alerts:reminders:skill:readwrite'])
                 .getResponse();
+        }*/
+        const permissions = handlerInput.requestEnvelope.context.System.user.permissions;
+        const consentToken = permissions && permissions.consentToken;
+        if(!consentToken) {
+            return handlerInput.responseBuilder
+                .addDirective({
+                    type: "Connections.SendRequest",
+                    name: "AskFor",
+                    payload: {
+                        "@type": "AskForPermissionsConsentRequest",
+                        "@version": "1",
+                        "permissionScope": "alexa::alerts:reminders:skill:readwrite"
+                    }, 
+                    token: ""
+                }).getResponse();
         }
+
         const speakOutput = 'MedicinesReminderIntentHandler';
         console.log('MedicinesRemindnerIntentHandler');
         return handlerInput.responseBuilder
