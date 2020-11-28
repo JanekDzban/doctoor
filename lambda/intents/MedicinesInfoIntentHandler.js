8f22260a20d6ba773467e76114a341630631bee3
Piotreue <piotr.guziorski@gmail.com>
2020-11-28 11:10:34 +0100

    Fixed handler code

--- lambda/intents/MedicinesInfoIntentHandler.js
+++ lambda/intents/MedicinesInfoIntentHandler.js
@@ -1,17 +1,17 @@
 const Alexa = require('ask-sdk-core');
 
-const MedicinesReminderIntentHandler = {
+const MedicinesInfoIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MedicinesReminderIntent';
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MedicinesInfoIntent';
     },
-    async handle(handlerInput) {
-        const speakOut = 'Tada!';
+    handle(handlerInput) {
+        const speakOutput = 'Tada!';
         return handlerInput.responseBuilder
-            .speak(speakOut)
-            .reprompt(speakOut)
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
 };
 
-module.exports = MedicinesReminderIntentHandler;
+module.exports = MedicinesInfoIntentHandler;
