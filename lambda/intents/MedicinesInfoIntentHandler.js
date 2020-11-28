c9c659fb28e927361bb10f2f281498c116da9725
Piotreue <piotr.guziorski@gmail.com>
2020-11-28 13:05:06 +0100

    Added mocked DB

--- lambda/intents/MedicinesInfoIntentHandler.js
+++ lambda/intents/MedicinesInfoIntentHandler.js
@@ -1,5 +1,9 @@
 const Alexa = require('ask-sdk-core');
 
+const MedicineInfoDB = JSON.parse(
+    '{"apap":{"description":"Nice apap drug!"}}'
+);
+
 const MedicinesInfoIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -8,24 +12,8 @@ const MedicinesInfoIntentHandler = {
     handle(handlerInput) {
         const drugName = handlerInput.requestEnvelope.request.intent.slots.medicineName.value;
         
-       	var myParams = { Bucket: '8e93a926-0a2a-4fdf-b1db-fd216179d85a-eu-west-1', Key: 'Media/Doctoor.MedicinesInfo/RPL.csv' };
-    	var AWS = require('aws-sdk'); var s3 = new AWS.S3();
-        
-                s3.getObject(myParams, function(err, data) { 
-           if(err) { 
-                 console.log(err, err.stack); }
-           else { 
-             var prompt = ''; 
-             var fileText = data.Body.toString(); 
-             prompt += fileText;
-             return handlerInput.responseBuilder 
-              .speak(prompt)
-              .getResponse();
-           } 
-        }); 
-        
         return handlerInput.responseBuilder
-            .speak(drugName)
+            .speak(MedicineInfoDB[drugName].description)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
