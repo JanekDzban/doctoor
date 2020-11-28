7c1dd02acf89e8dfe697f2fd8799c48ccff92a14
Piotreue <piotr.guziorski@gmail.com>
2020-11-28 13:32:33 +0100

    Test s3 content query"

--- lambda/intents/MedicinesInfoIntentHandler.js
+++ lambda/intents/MedicinesInfoIntentHandler.js
@@ -1,8 +1,6 @@
 const Alexa = require('ask-sdk-core');
+const MedicinesInfoDB = require('./MedicinesInfoDB.js')
 
-const MedicineInfoDB = JSON.parse(
-    '{"apap":{"description":"Nice apap drug!"}}'
-);
 
 const MedicinesInfoIntentHandler = {
     canHandle(handlerInput) {
@@ -13,10 +11,73 @@ const MedicinesInfoIntentHandler = {
         const drugName = handlerInput.requestEnvelope.request.intent.slots.medicineName.value;
         
         return handlerInput.responseBuilder
-            .speak(MedicineInfoDB[drugName].description)
+            .speak(lookupMedicine(drugName))
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
+};
+
+const lookupMedicine = (medicineName) => {
+    return new Promise((resolve, reject) => {
+        try {
+            const bucketName = process.env.S3_PERSISTENCE_BUCKET;
+            const keyName = 'RPL.csv';
+            const query = `SELECT id from S3Object s WHERE s.name ='${medicineName}' LIMIT 1`;
+            let returnVal = 0;
+            
+            const params = {
+              Bucket: bucketName,
+              Key: keyName,
+              ExpressionType: 'SQL',
+              Expression: query,
+              InputSerialization: {
+                CSV: {
+                  FileHeaderInfo: 'USE',
+                },
+                CompressionType: 'NONE',
+              },
+              OutputSerialization: {
+                CSV: {
+                },
+              }
+            };
+            
+            console.log('start select');
+            s3.selectObjectContent(params, (err, data) => {
+            	if (err) {
+            		reject(0);
+            	}
+            
+            	const eventStream = data.Payload;
+            	
+            	eventStream.on('data', (event) => {
+            		if (event.Records) {
+            			returnVal = event.Records.Payload.toString();
+            			resolve(returnVal);
+            		} else if (event.Stats) {
+            			//console.log(`Processed ${event.Stats.Details.BytesProcessed} bytes`);
+            		} else if (event.End) {
+            		    //console.log('SelectObjectContent completed');
+            		}
+            	});
+            
+            	// Handle errors encountered during the API call
+            	eventStream.on('error', (err) => {
+            		switch (err.name) {
+            			// Check against specific error codes that need custom handling
+            		}
+            	});
+            	eventStream.on('end', () => {
+            		// Finished receiving events from S3
+            		console.log(`returning: ${returnVal}`);
+            	    resolve(returnVal);
+            	});
+            });
+        } catch (e) {
+            console.log(e);
+            reject(0);
+        }
+    })
 };
 
 module.exports = MedicinesInfoIntentHandler;
