eed73f57b93d475c5417eba1a861ebbe304b2263
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-28 11:59:23 +0000

    updated lambda/intents/MedicinesInfoIntentHandler.js.

--- lambda/intents/MedicinesInfoIntentHandler.js
+++ lambda/intents/MedicinesInfoIntentHandler.js
@@ -11,6 +11,19 @@ const MedicinesInfoIntentHandler = {
        	var myParams = { Bucket: '8e93a926-0a2a-4fdf-b1db-fd216179d85a-eu-west-1', Key: 'Media/Doctoor.MedicinesInfo/RPL.csv' };
     	var AWS = require('aws-sdk'); var s3 = new AWS.S3();
         
+                s3.getObject(myParams, function(err, data) { 
+           if(err) { 
+                 console.log(err, err.stack); }
+           else { 
+             var prompt = ''; 
+             var fileText = data.Body.toString(); 
+             prompt += fileText;
+             return handlerInput.responseBuilder 
+              .speak(prompt)
+              .getResponse();
+           } 
+        }); 
+        
         return handlerInput.responseBuilder
             .speak(drugName)
             //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
