bfa741c6fdbf8edb874f75e20045b2ab4531a42d
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-11-28 20:18:46 +0000

    updated lambda/intents/MedicinesInfoIntentHandler.js.

--- lambda/intents/MedicinesInfoIntentHandler.js
+++ lambda/intents/MedicinesInfoIntentHandler.js
@@ -1,14 +1,5 @@
 const Alexa = require('ask-sdk-core');
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://drugdb:drugdbadmin@cluster0.zoed5.mongodb.net?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-    const collection = client.db("drugsDB").collection("drugsInfo");
-    console.log(collection.countDocuments());
-    client.close();
-});
-
 const MedicinesInfoIntentHandler = {
     canHandle(handlerInput) {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -16,28 +7,10 @@ const MedicinesInfoIntentHandler = {
     },
     handle(handlerInput) {
         const drugName = handlerInput.requestEnvelope.request.intent.slots.medicineName.value;
-        
-        MongoClient.connect(uri, function (err, db) {
 
-            db.collection('drugsInfo', function (err, collection) {
-                const query = { "name": { "$eq": drugName } };
-                var drugInfo = collection.findOne(query)
-                .then(result => {
-                  if(result) {
-                    console.log(`Successfully found document: ${result}.`);
-                  } else {
-                    console.log("No document matches the provided query.");
-                  }
-                  return result;
-                })
-                .catch(err => console.error(`Failed to find document: ${err}`));
-
-            });
-
-        });
 
         return handlerInput.responseBuilder
-            .speak(drugInfo.commonName)
+            .speak(drugName)
             .reprompt('add a reprompt if you want to keep the session open for the user to respond')
             .getResponse();
     }
